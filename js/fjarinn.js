19b74f7b2a10c934df5696dc092802c6af9906d2
Halldór Eldjárn <halldor@eldjarn.net>
2014-02-05 17:25:12 +0000

    Fixes to UI

--- js/fjarinn.js
+++ js/fjarinn.js
@@ -2,12 +2,21 @@ $(document).ready(function () {
 
 	var meira_btn = $("#meira-lan");
 	var meira_tmp = $("#lan-tmp").html();
+	var remove_btn = $("button.remove");
 	var lana_list = $("#lanalisti");
+
+	lana_list.delegate('button.remove', 'click', function (e) {
+		$(this).parent().parent().slideToggle(100, function () {this.remove();});
+	});
 	
 	var meira_compiled = _.template(meira_tmp);
 	
 	meira_btn.click(function() {
 		//console.log("clicked");
-		lana_list.prepend(meira_compiled
-	)});
+		lana_list.append(meira_compiled);
+	});
+
+	remove_btn.click(function() {
+		this.parent().remove();
+	})
 });
